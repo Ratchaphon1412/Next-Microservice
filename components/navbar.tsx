7f0e0f4cdf47df24912ef42fc76aaa06d25d5372
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-12-11 00:06:59 +0700

    change route register button to register page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -49,7 +49,7 @@ export default function component() {
               <div className="border-l-2 border-gray-400 h-[30px]"></div>
               <a
                 className="items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
-                href="/auth/login"
+                href="/auth/register"
               >
                 Sign in
               </a>
