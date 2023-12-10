951a5c5938d4dbe8368560599591290b5628dbc0
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-12-10 16:46:07 +0700

    create .env and config

--- lib/useApi.ts
+++ lib/useApi.ts
@@ -1,9 +1,8 @@
 async function loadPosts() {
-    // Call an external API endpoint to get posts
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await res.json()
-    return data
+  // Call an external API endpoint to get posts
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return data;
 }
 
-
-export { loadPosts }
+export { loadPosts };
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    publicRuntimeConfig: {
+        // Will be available on both server and client
+        base_url: process.env.BASE_URL,
+    },
+}
+
 
 module.exports = nextConfig
