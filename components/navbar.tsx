cf4ef8e0a93daf6c4a23cf7671b252d6bf69fbac
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-12-11 17:02:07 +0700

    edit navbar and front

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import Logo from '@/components/logo';
 
 export default function component() {
     return (
-      <header className="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md bg-transparent py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg">
+      <header className="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md bg-black py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg">
         <div className="px-4">
           <div className="flex items-center justify-between">
             <div className="flex shrink-0">
@@ -13,7 +13,7 @@ export default function component() {
                   alt=""
                 /> */}
                 <Logo class="h-5" />
-                <p className="text-black ">Pixel Man</p>
+                <p className="text-white font-pixelletMedium">Pixel Man</p>
               </a>
             </div>
   
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,32 @@
-import type { Config } from 'tailwindcss'
-import daisyui from 'daisyui'
+import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 const config: Config = {
   content: [
     "./node_modules/flowbite-react/**/*.js",
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./public/**/*.html",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        //  sans:['Pixellet'],
+        pixellet: ["Pixellet"],
+        pixelletMedium: ["PixelletMedium"],
+        pixelletBold: ["PixelletBold"],
+        pixelletItalic: ["PixelletItalic"],
+        pixelletBoldItalic: ["PixelletBoldItalic"],
+        pixelletCircle: ["PixelletCircle"],
       },
     },
   },
-  plugins: [
-    daisyui,
-    require("flowbite/plugin"),
-  ],
-  
-}
-export default config
+  plugins: [daisyui, require("flowbite/plugin")],
+};
+export default config;
