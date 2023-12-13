ef69f0e93380e8bcde4a2e06646046448c963b67
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-12-13 15:35:38 +0700

    create page gallery

---
+++ app/gallery/page.tsx
@@ -0,0 +1,11 @@
+import FourthSection from "@/app/components/fourth-section";
+import FifthSection from "@/app/components/clip-path";
+export default function GalleryPage() {
+  return (
+    <main className="overflow-hidden">
+      {/* <FifthSection /> */}
+      <FourthSection />
+      <FourthSection />
+    </main>
+  );
+}
