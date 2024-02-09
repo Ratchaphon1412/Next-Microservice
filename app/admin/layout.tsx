0779271ceab615d6b04bcebda1e7f1ba29e65c0d
kornavit <kornavit.p@ku.th>
2024-02-09 21:48:48 +0700

    add the detail order

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
 
                 {/* <!-- ===== Main Content Start ===== --> */}
                 <main>
-                  <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                  <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 mt-14">
                     {children}
                   </div>
                 </main>
